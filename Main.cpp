171da789d254c2bf9910a338d16cbecb3d65d337
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-01-31 09:20:23 +0530

    Let's Start the game

---
+++ Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+////class
+//class Pokemon
+//{
+//public:
+//    void attack()
+//    {
+//        cout << "Pokemon attacks \n";
+//    }
+//};
+
+int main()
+{
+
+
+	cout << " lets start the game";
+
+
+
+};
